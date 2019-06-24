6a169711f942748b166a566bbfd17527ae06918a
sirusb <djek.nad@gmail.com>
2019-06-23 21:46:05 -0400

    Update filter_readNotInGenomicRegions.groovy

--- Mapping/scRNASeq/filter_readNotInGenomicRegions.groovy
+++ Mapping/scRNASeq/filter_readNotInGenomicRegions.groovy
@@ -11,8 +11,8 @@ get_nonGenomicReads = {
 
 extractReads  = {
 
-  def read1 = "/nfs2/nadhir/Projects/201804_xiaoji_Dux/fastq/0H/read1.fastq.gz"
-  def read2 = "/nfs2/nadhir/Projects/201804_xiaoji_Dux/fastq/0H/read2.fastq.gz"
+  var read1 : "/nfs2/nadhir/Projects/201804_xiaoji_Dux/fastq/0H/read1.fastq.gz"
+  var read2 : "/nfs2/nadhir/Projects/201804_xiaoji_Dux/fastq/0H/read2.fastq.gz"
   exec """
     seqtk subseq ${read1} ${input} > read1.fastq &&
     seqtk subseq ${read2} ${input} > read2.fastq
